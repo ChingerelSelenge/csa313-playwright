dca0079dbed1be50502b9fc997c21153c93d39d1
grle0e <chingerel592@gmail.com>
2026-09-08 22:51:41 +0800

    Add cart interaction test

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -28,3 +28,12 @@ test('Буруу нууц үгээр нэвтрэх', async ({ page }) => {
 
   await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
+
+test('бараа сагслах', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Add to cart' }).first().click();
+  await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+});
