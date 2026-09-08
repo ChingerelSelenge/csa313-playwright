a516a191b0e5736fae5570cef78b71ca584e2a0a
grle0e <chingerel592@gmail.com>
2026-09-08 22:22:47 +0800

    Add login test

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -9,6 +9,22 @@ test('Амжилттай нэвтрэх', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Open Menu' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
-  
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
+
+test('Буруу нууц үгээр нэвтрэх', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('wrong_password');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(
+    page.getByText(
+      'Epic sadface: Username and password do not match any user in this service',
+      { exact: true }
+    )
+  ).toBeVisible();
+
   await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
